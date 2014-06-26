2c092f68ffb8207a6948a423de561f51fbf07992
Martin Riedel <martin.riedel@innoactive.de>
2014-06-26 21:21:28 +0200

    project scaffold

---
+++ app/scripts/main/main-ctrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('foodNinja')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
